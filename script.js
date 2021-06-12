9cf1826ab7e5117f8b396050d1e0e68fd23e32a1
Saskia vd Kooij <84019788+saskiavanderkooij@users.noreply.github.com>
2021-06-12 10:53:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const leeftijd = 41;
+if (leeftijd > 18) {
+    console.log("Welkom.");
+} else {
+    console.log("Je bent nog te jong . Tot over een paar jaar!");
+} 
+
+const isVrouw = false;
+if (isVrouw) {
+    console.log("Welkom op ladiesnight.");
+} else {
+    console.log("Helaas. Je kan geen kaarten kopen voor de ladiesnight.");
+}
+
+const driverStatus = "zat";
+if (driverStatus == "bob") {
+    console.log("Je kan veilig naar huis rijden.");
+} else {
+    console.log("We gaan een taxi voor je bellen.");
+}
